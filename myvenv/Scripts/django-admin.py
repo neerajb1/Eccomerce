9cf1b09eadb937709158541b290a830e845c92d2
Neeraj <shuklaneeraj2014.2015@com>
2018-01-18 17:59:39 +0530

    last commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\neeraj\desktop\project\ecommerce-2-master\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
